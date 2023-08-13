ccb88bc207dca9a253d6ca5e30681f1606a8a2b5
Shubham maurya <49495079+Ermaurya@users.noreply.github.com>
2023-08-13 10:35:17 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,33 +1,55 @@
 import Card from './component/Card';
 import './App.css';
+import Comp from './component/Comp';
 
 function App() {
   return (
     <div>
-      <Card name='Bright' subtitle='2017, David Ayer'
+     
+     
+      
+      <Card 
+      name='Black Panther' 
         description=' Set in a world where fantasy creatures live side by side with humans. A human cop is forced to work with an Orc to find a weapon everyone is prepared to kill for.'
-        image='https://movieplayer.net-cdn.it/t/images/2017/12/20/bright_jpg_191x283_crop_q85.jpg'
-        background='https://occ-0-2433-448.1.nflxso.net/art/cd5c9/3e192edf2027c536e25bb5d3b6ac93ced77cd5c9.jpg'
-        time='117 min'
-        m_type='Action, Crime, Fantasy'
-        rating='⭐⭐⭐⭐'
+        image='https://sb.kaleidousercontent.com/67418/960x550/d1e78c2a25/individuals-a.png'
+        time='13 min'
+        m_type='Action'
+       
       />
-      <Card name='Tomb Raider' subtitle='2018, Roar Uthaug'
-        description='Lara Croft, the fiercely independent daughter of a missing adventurer, must push herself beyond her limits when she finds herself on the island where her father disappeared.'
-        image='https://mr.comingsoon.it/imgdb/locandine/235x336/53750.jpg'
-        background='https://fsmedia.imgix.net/cd/c9/5e/ba/4817/4d9a/93f0/c776ec32ecbc/lara-crofts-neck-looks-unnatural-in-the-new-poster-for-tomb-raider.png'
-        time='125 min'
-        m_type='Action, Fantasy'
-        rating='⭐⭐⭐⭐'
-      />
-      <Card name='Black Panther' subtitle='2018, Ryan Coogler'
+      <Card 
+      name='Black Panther' 
         description=' Set in a world where fantasy creatures live side by side with humans. A human cop is forced to work with an Orc to find a weapon everyone is prepared to kill for.'
-        image='https://mr.comingsoon.it/imgdb/locandine/235x336/53715.jpg'
-        background='https://www.gannett-cdn.com/-mm-/c03fd140debe8ad4c05cf81a5cad7ad61a12ce52/c=0-1580-2985-3266&r=x803&c=1600x800/local/-/media/2017/06/09/USATODAY/USATODAY/636326272873599176-Black-Panther-Teaser.jpg'
-        time='134 min'
-        m_type='Action, Adventure, Sci-Fi'
-        rating='⭐⭐⭐⭐'
+        image='https://imgv3.fotor.com/images/homepage-feature-card/upload-a-girl-picture.jpg'
+        time='13 min'
+        m_type='Action'
+       
       />
+      <Card 
+      name='Black Panther' 
+        description=' Set in a world where fantasy creatures live side by side with humans. A human cop is forced to work with an Orc to find a weapon everyone is prepared to kill for.'
+        image='https://imgv3.fotor.com/images/blog-cover-image/png-background-banner.webp'
+        time='13 min'
+        m_type='Action'
+       
+      />
+      <Card 
+      name='Black Panther' 
+        description=' Set in a world where fantasy creatures live side by side with humans. A human cop is forced to work with an Orc to find a weapon everyone is prepared to kill for.'
+        image='https://asset-2.tstatic.net/kaltim/foto/bank/images/cara-mengubah-background-pas-foto-tangkapan-layar-removebg.jpg'
+        time='13 min'
+        m_type='Action'
+       
+      />
+      <Card 
+      name='Black Panther' 
+        description=' Set in a world where fantasy creatures live side by side with humans. A human cop is forced to work with an Orc to find a weapon everyone is prepared to kill for.'
+        image='https://gamedva.com/wp-content/uploads/2022/02/Background-Eraser-Remove-BG.jpg'
+        time='13 min'
+        m_type='Action'
+       
+      />
+
+     
     </div>
   );
 }
--- src/component/Card.js
+++ src/component/Card.js
@@ -4,33 +4,40 @@ import {faHeart, faMessage, faShareNodes } from '@fortawesome/free-solid-svg-ico
 const Card = (props) => {
     return (
         <>
-            <div className="movie_card bright">
+            {/* <div className="movie_card bright"> */}
                 <div className="info_section">
                     <div className="movie_header">
-                        <img className="locandina" alt="" src={props.image} />
+                        <div className='text_area'>
+                          <div className='users'>
+                            <img className='dp_img' alt="" src={props.image} />
+                            <span className='user_text'>{props.name}</span>
+                          </div>
                         <h1>{props.name}</h1>
                         <h4>{props.subtitle}</h4>
-                        <span className="minutes">{props.time}</span>
-                        <p className="type">{props.m_type}</p>
+                       
                         <p>{props.rating}</p>
-                    </div>
-                    <div className="movie_desc">
+                        <div className="movie_desc">
                         <p className="text">
                             {props.description}
                         </p>
+                        <span className="minutes">{props.time}</span>
+                        <p className="type">{props.m_type}</p>
+                        
                     </div>
-                    <div className="movie_social ">
-                        <ul>
-                            <li><i className="material-icons"><FontAwesomeIcon icon={faShareNodes}/></i></li>
-                            <li><i className="material-icons"><FontAwesomeIcon icon={faHeart}/></i></li>
-                            <li><i className="material-icons"><FontAwesomeIcon icon={faMessage}/></i></li>
-                        </ul>
+                        </div>
+                        <div className='img_field'>
+
+                        <img className="locandina" alt="" src={props.image} />
+                        </div>
                     </div>
+                   
+                    
                 </div>
-                <div className="blur_back bright_back">
+                {/* <div className="blur_back bright_back">
                     <img src={props.background} alt=''/>
-                </div>
-            </div>
+                </div> */}
+            {/* </div> */}
+            
         </>
     )
 }
